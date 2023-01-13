820c74ddaac6b80fa839d14f201120b102ee3bd5
Zoey1318 <gyulikimwork@gmail.com>
2023-01-13 06:05:26 +0000

    working on home page js function #1

--- js/main.js
+++ js/main.js
@@ -18,8 +18,20 @@ function genreEntry() {
   genrePage.classList.toggle('hidden');
 }
 
+var random = ['images/genre/adventure poster.jpg', 'images/genre/action poster.jpg', 'images/genre/comedy poster.jpg'];
+const genreTagBox = document.querySelector('.banner-img-box');
+const genreTagImg = document.createElement('img');
+genreTagImg.setAttribute('class', 'banner-img');
+genreTagImg.setAttribute('alt', 'banner');
+
 function homeEntry() {
   homePage.classList.toggle('hidden');
+}
+
+function randomEntry() {
+  const randomIndex = Math.floor(Math.random() * random.length);
+  genreTagImg.setAttribute('src', random[randomIndex]);
+  genreTagBox.appendChild(genreTagImg);
 }
 
 /* main page */
@@ -35,6 +47,7 @@ profile.addEventListener('click', function () {
 });
 
 /* genre page */
+
 previousBtn.addEventListener('click', function () {
   genreEntry();
   mainEntry();
@@ -43,6 +56,7 @@ previousBtn.addEventListener('click', function () {
 skipBtn.addEventListener('click', function () {
   genreEntry();
   homeEntry();
+  randomEntry();
 });
 
 const genreListAll = document.querySelector('.genre-list');
@@ -60,18 +74,21 @@ genreListAll.addEventListener('click', function (event) {
   }
 });
 
-var genreTagData = [];
-
-function renderGenre(genre) {
-  for (let i = 0; i < genreList.length; i++) {
-    if (genreTitle[i].getAttribute('class') === 'genre-title selected') {
-      genreTagData.push(genreTitle[i].textContent);
-    }
-  }
-}
+const tagListAll = document.querySelector('.genre-tag');
 
 checkBtn.addEventListener('click', function () {
   genreEntry();
   homeEntry();
-  renderGenre();
+  for (let i = 0; i < genreList.length; i++) {
+    if (genreTitle[i].getAttribute('class') === 'genre-title selected') {
+      data.genreTagEntry.push(genreTitle[i].textContent);
+      const tagList = document.createElement('li');
+      tagList.setAttribute('class', 'genre-tag-inner');
+      tagList.textContent = genreTitle[i].textContent;
+      tagListAll.appendChild(tagList);
+    }
+  }
+  return randomEntry();
 });
+
+/* home page */
